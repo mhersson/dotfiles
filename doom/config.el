d0b03950024d9b04b3bf760e8ca5c04611a9c908
Morten Hersson <mhersson@gmail.com>
2020-09-17 09:54:07 +0200

    Changed font to Iconsolata

--- doom/config.el
+++ doom/config.el
@@ -21,7 +21,7 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "UbuntuMono Nerd Font" :size 17))
+(setq doom-font (font-spec :family "Inconsolata" :size 18))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -81,7 +81,7 @@
   (setq lsp-gopls-complete-unimported t)
   (setq flycheck-checker 'golangci-lint)
   (setq flycheck-golangci-lint-enable-all t)
-  (setq flycheck-golangci-lint-disable-linters '("gochecknoinits" "gochecknoglobals" "gomnd" "gofumpt")))
+  (setq flycheck-golangci-lint-disable-linters '("gochecknoinits" "gochecknoglobals" "gomnd" "gofumpt" "gci")))
 
 ;; Insert CLOSED property when a TODO item is marked as DONE
 (setq org-log-done 'time)
