f3cb5e67b19dd333ea4371ab00efe5592b4b07c4
Morten Hersson <mher@stibodx.com>
2021-04-13 10:09:00 +0200

    Only show cluster from current context

--- tmux/get-ocp-cluster.sh
+++ tmux/get-ocp-cluster.sh
@@ -3,9 +3,12 @@ set -euo pipefail
 
 if (oc whoami 1>&2 >/dev/null) ; then
   status=$(oc adm config current-context |
-    awk -F'/' '{ print $3 "@" $2 ":" $1}' |
-    awk -F':' '{ print $1 "/" $3 }' |
+    awk -F'/' '{ print $2 }' |
+    awk -F':' '{ print $1 }' |
     sed "s/ocp-\|api-\|-openshift-cue-cloud\|@stibodx.com//"g)
+    #awk -F'/' '{ print $3 "@" $2 ":" $1}' |
+    #awk -F':' '{ print $1 "/" $3 }' |
+    #sed "s/ocp-\|api-\|-openshift-cue-cloud\|@stibodx.com//"g)
 
-  echo -n "#[fg=colour31]#[bg=colour31,fg=colour236,bold] ${status} #[bg=colour31,fg=colour237,nobold]"
+  echo -n "#[fg=colour214]#[bg=colour214,fg=colour236,bold] ${status} #[bg=colour214,fg=colour237,nobold]"
 fi
