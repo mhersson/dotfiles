18b76f8acc467ca0bb0444759293a80b0e9e90fe
Morten Hersson <mher@stibodx.com>
2021-06-30 20:33:21 +0200

    Renamed script to get-vpn-status.sh

---
+++ tmux/get-vpn-status.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# set -euo pipefail
+
+# Gruvbox
+# if (piactl get connectionstate | grep Connected >/dev/null 2>&1); then
+#     echo -n "#[fg=colour236]#[bg=colour236,fg=colour248] #h "
+# else
+#     echo -n "#[fg=colour196]#[bg=colour196,fg=colour236,bold] #h"
+# fi
+
+# Nord
+# if (piactl get connectionstate | grep Connected >/dev/null 2>&1); then
+#     echo -n "#[fg=black,bg=cyan,bold] #h "
+# else
+#     echo -n "#[fg=black,bg=red,bold] #h"
+# fi
+
+if (nmcli c show --active | grep protonvpn.com.udp >/dev/null 2>&1); then
+    echo -n "#[fg=black,bg=cyan,bold] #h "
+else
+    echo -n "#[fg=black,bg=red,bold] #h"
+fi
+
