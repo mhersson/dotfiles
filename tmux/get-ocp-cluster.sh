f1c7ca0a7d3dbb496d0f638d25d3e5f6f2140790
Morten Hersson <mher@stibodx.com>
2021-09-03 17:46:49 +0200

    Minor formatting and color changes

--- tmux/get-ocp-cluster.sh
+++ tmux/get-ocp-cluster.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if (oc whoami 1>&2 >/dev/null) ; then
+if (oc whoami 1>&2 >/dev/null); then
   status=$(oc adm config current-context |
     awk -F'/' '{ print $2 }' |
     awk -F':' '{ print $1 }' |
     sed "s/ocp-\|api-\|-openshift-cue-cloud\|@stibodx.com//"g)
-    #awk -F'/' '{ print $3 "@" $2 ":" $1}' |
-    #awk -F':' '{ print $1 "/" $3 }' |
-    #sed "s/ocp-\|api-\|-openshift-cue-cloud\|@stibodx.com//"g)
+  #awk -F'/' '{ print $3 "@" $2 ":" $1}' |
+  #awk -F':' '{ print $1 "/" $3 }' |
+  #sed "s/ocp-\|api-\|-openshift-cue-cloud\|@stibodx.com//"g)
 
-  echo -n "#[fg=colour214]#[bg=colour214,fg=colour236,bold] ${status} #[bg=colour214,fg=colour237,nobold]"
+  echo -n "#[fg=colour214]#[bg=colour214,fg=colour242] ${status} #[bg=colour214,fg=default]"
 fi
