f2112b9ffa13e15991d85a4e2056239ddd89d5dc
Morten Hersson <mher@stibodx.com>
2022-03-25 19:06:16 +0100

    If piactl is installed used pia, else proton

--- tmux/ocp.sh
+++ tmux/ocp.sh
@@ -8,11 +8,6 @@ if (oc whoami 1>&2 >/dev/null); then
     awk -F'/' '{ print $2 }' |
     awk -F':' '{ print $1 }' |
     sed "s/ocp-\|api-\|-openshift-cue-cloud\|@stibodx.com//"g)
-  #awk -F'/' '{ print $3 "@" $2 ":" $1}' |
-  #awk -F':' '{ print $1 "/" $3 }' |
-  #sed "s/ocp-\|api-\|-openshift-cue-cloud\|@stibodx.com//"g)
-
-  #echo -n "#[fg=colour214]#[bg=colour214,fg=colour242] ${status} #[bg=colour214,fg=default]"
 
   case "$theme" in
     "github")
--- tmux/vpn.sh
+++ tmux/vpn.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 # set -euo pipefail
 
-theme=solarized
+theme=github
+
+if (command -v piactl >/dev/null); then
+  VPN_CMD="piactl get connectionstate | grep -i connected >/dev/null"
+else
+  VPN_CMD="protonvpn-cli status | grep -i country >/dev/null"
+fi
 
 case "$theme" in
-  "solarized")
-    if (protonvpn-cli status | grep -i country >/dev/null); then
+  "github")
+    if (eval $VPN_CMD); then
       echo -n "#[bg=default,fg=colour67]#[bg=colour67,fg=colour246] 🔐 "
     else
       echo -n "#[bg=default,fg=red]#[bg=red,fg=colour246] 🔓 "
     fi
     ;;
   *)
-    if (protonvpn-cli status | grep -i country >/dev/null); then
+    if (eval $VPN_CMD); then
       echo -n "#[bg=colour236,fg=colour239]#[bg=colour239,fg=colour246] 🔐 "
     else
       echo -n "#[bg=colour236,fg=red]#[bg=red,fg=colour246] 🔓 "
