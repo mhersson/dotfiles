a7068cddd9f3bf28b5190834c229a6d3b242444b
Morten Hersson <mher@stibodx.com>
2022-05-26 21:59:17 +0200

    Minor color adjustments

--- sway/waybar/vpn.sh
+++ sway/waybar/vpn.sh
@@ -2,8 +2,13 @@
 
 class="down"
 
-country=$(protonvpn-cli status | grep Country | awk '{print $2}')
-if [ "${country}" != "" ];then
+if (command -v piactl >/dev/null); then
+  VPN_CMD="piactl get connectionstate | grep -i connected >/dev/null"
+else
+  VPN_CMD="protonvpn-cli status | grep -i country >/dev/null"
+fi
+
+if (eval $VPN_CMD); then
   class="up"
 fi
 
