56605682b7e4d622b992064bc28b3e06f8b24055
Morten Hersson <mher@stibodx.com>
2022-08-11 06:07:35 +0200

    Disable the commit confirmation in neogit

--- nvim/lua/plugins/config/neogit.lua
+++ nvim/lua/plugins/config/neogit.lua
@@ -1,4 +1,10 @@
 local status_ok, neogit = pcall(require, "neogit")
 if not status_ok then return end
 
-neogit.setup()
+neogit.setup({
+  disable_commit_confirmation = true,
+  use_magit_keybindings = false,
+  integrations = {
+    diffview = false
+  },
+})
