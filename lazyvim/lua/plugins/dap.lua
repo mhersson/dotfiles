d9af23f083c0d932bfce328dadf54bffa68bb838
Morten Hersson <mher@stibodx.com>
2023-03-15 14:04:05 +0100

    Added debug config for go

---
+++ lazyvim/lua/plugins/dap.lua
@@ -0,0 +1,76 @@
+return {
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+    },
+    config = function(_, opts)
+      require("dapui").setup(opts)
+      local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+    keys = {
+      {
+        "<leader>db",
+        function()
+          require("dap").toggle_breakpoint()
+        end,
+        desc = "Toggle Breakpoint",
+      },
+      {
+        "<leader>dc",
+        function()
+          require("dap").continue()
+        end,
+        desc = "Continue",
+      },
+      {
+        "<leader>di",
+        function()
+          require("dap").step_into()
+        end,
+        desc = "Step Into",
+      },
+      {
+        "<leader>do",
+        function()
+          require("dap").step_over()
+        end,
+        desc = "Step Over",
+      },
+      {
+        "<leader>dO",
+        function()
+          require("dap").step_out()
+        end,
+        desc = "Step Out",
+      },
+      {
+        "<leader>dt",
+        function()
+          require("dap").debug_test()
+        end,
+        desc = "Debug Test",
+      },
+      {
+        "<leader>dl",
+        function()
+          require("dap").debug_last_test()
+        end,
+        desc = "Debug Last Test",
+      },
+    },
+  },
+  {
+    "leoluz/nvim-dap-go",
+    config = true,
+  },
+}
