138dd449ecaf2aff3aa0b216aa026564f476a471
Morten Hersson <mher@stibodx.com>
2023-03-30 15:22:23 +0200

    Added plugin toggleterm

---
+++ lazyvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      open_mapping = [[<c-\>]],
+      direction = "float",
+      float_opts = {
+        border = "curved",
+      },
+      highlights = {
+        FloatBorder = {
+          guifg = "#8AADF4",
+        },
+      },
+    },
+    config = function(_, opts)
+      require("toggleterm").setup(opts)
+    end,
+  },
+}
