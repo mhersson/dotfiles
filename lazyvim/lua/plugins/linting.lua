6b4bb6765d573f2468c9bb4ccab9adbe0873cb74
Morten Hersson <mher@stibodx.com>
2023-04-11 12:26:55 +0200

    Specify null-ls formatting

--- lazyvim/lua/plugins/linting.lua
+++ lazyvim/lua/plugins/linting.lua
@@ -4,6 +4,25 @@ return {
     "jose-elias-alvarez/null-ls.nvim",
     opts = function(_, opts)
       local nls = require("null-ls")
+      local formatting = nls.builtins.formatting
+      opts.sources = {
+        formatting.shfmt.with({
+          extra_args = { "-i", "2", "-ci", "-bn" },
+        }),
+        formatting.prettier.with({
+          filetypes = { "html", "json", "markdown" },
+        }),
+        formatting.yamlfmt.with({
+          filetypes = { "yaml" },
+        }),
+        formatting.nixfmt.with({
+          filetypes = { "nix" },
+        }),
+        formatting.black.with({
+          filetypes = { "python" },
+        }),
+      }
+
       opts.sources = vim.list_extend(opts.sources, { nls.builtins.diagnostics.golangci_lint })
       -- opts.sources = vim.list_extend(opts.sources, { nls.builtins.diagnostics.shellcheck })
     end,
--- lazyvim/lua/plugins/toggleterm.lua
+++ lazyvim/lua/plugins/toggleterm.lua
@@ -3,7 +3,7 @@ return {
     "akinsho/toggleterm.nvim",
     version = "*",
     opts = {
-      open_mapping = [[<c-\>]],
+      open_mapping = [[<c-`>]],
       direction = "float",
       float_opts = {
         border = "curved",
