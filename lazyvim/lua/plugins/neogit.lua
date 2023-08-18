d9c6f08c56c7fbf7c723d17bfdc5076b8659f8de
Morten Hersson <mhersson@gmail.com>
2023-08-18 10:34:06 +0200

    Update neogit config from upstream

--- lazyvim/lua/plugins/neogit.lua
+++ lazyvim/lua/plugins/neogit.lua
@@ -12,61 +12,168 @@ return {
       },
     },
     opts = {
-      disable_signs = false,
       disable_hint = false,
       disable_context_highlighting = false,
+      disable_signs = false,
       disable_commit_confirmation = true,
-      -- Neogit refreshes its internal state after specific events, which can be expensive depending on the repository size.
-      -- Disabling `auto_refresh` will make it so you have to manually refresh the status after you open it.
-      auto_refresh = true,
       disable_builtin_notifications = false,
-      use_magit_keybindings = false,
-      -- Change the default way of opening neogit
+      telescope_sorter = function()
+        return nil
+      end,
+      disable_insert_on_commit = true,
+      use_per_project_settings = true,
+      remember_settings = true,
+      auto_refresh = true,
+      sort_branches = "-committerdate",
       kind = "tab",
       -- The time after which an output console is shown for slow running commands
       console_timeout = 2000,
       -- Automatically show console if a command takes more than console_timeout milliseconds
       auto_show_console = true,
-      -- Change the default way of opening the commit popup
-      commit_popup = {
-        kind = "split_above",
+      status = {
+        recent_commit_count = 10,
       },
-      -- Change the default way of opening popups
+      commit_editor = {
+        kind = "split",
+      },
+      commit_select_view = {
+        kind = "tab",
+      },
+      commit_view = {
+        kind = "vsplit",
+      },
+      log_view = {
+        kind = "tab",
+      },
+      rebase_editor = {
+        kind = "split",
+      },
+      reflog_view = {
+        kind = "tab",
+      },
+      merge_editor = {
+        kind = "split",
+      },
+      preview_buffer = {
+        kind = "split",
+      },
       popup = {
         kind = "split",
       },
-      -- customize displayed signs
       signs = {
-        -- { CLOSED, OPENED }
-        section = { ">", "v" },
-        item = { ">", "v" },
         hunk = { "", "" },
+        item = { ">", "v" },
+        section = { ">", "v" },
       },
       integrations = {
-        diffview = false,
+        telescope = nil,
+        diffview = nil,
       },
-      -- Setting any section to `false` will make the section not render at all
       sections = {
+        sequencer = {
+          folded = false,
+          hidden = false,
+        },
         untracked = {
           folded = false,
+          hidden = false,
         },
         unstaged = {
           folded = false,
+          hidden = false,
         },
         staged = {
           folded = false,
+          hidden = false,
         },
         stashes = {
           folded = true,
+          hidden = false,
         },
-        unpulled = {
+        unpulled_upstream = {
           folded = true,
+          hidden = false,
         },
-        unmerged = {
+        unmerged_upstream = {
           folded = false,
+          hidden = false,
+        },
+        unpulled_pushRemote = {
+          folded = true,
+          hidden = false,
+        },
+        unmerged_pushRemote = {
+          folded = false,
+          hidden = false,
         },
         recent = {
-          folded = false,
+          folded = true,
+          hidden = false,
+        },
+        rebase = {
+          folded = true,
+          hidden = false,
+        },
+      },
+      ignored_settings = {
+        "NeogitPushPopup--force-with-lease",
+        "NeogitPushPopup--force",
+        "NeogitPullPopup--rebase",
+        "NeogitCommitPopup--allow-empty",
+        "NeogitRevertPopup--no-edit", -- TODO: Fix incompatible switches with default enables
+      },
+      mappings = {
+        finder = {
+          ["<cr>"] = "Select",
+          ["<c-c>"] = "Close",
+          ["<esc>"] = "Close",
+          ["<c-n>"] = "Next",
+          ["<c-p>"] = "Previous",
+          ["<down>"] = "Next",
+          ["<up>"] = "Previous",
+          ["<tab>"] = "MultiselectToggleNext",
+          ["<s-tab>"] = "MultiselectTogglePrevious",
+          ["<c-j>"] = "NOP",
+        },
+        status = {
+          ["q"] = "Close",
+          ["I"] = "InitRepo",
+          ["1"] = "Depth1",
+          ["2"] = "Depth2",
+          ["3"] = "Depth3",
+          ["4"] = "Depth4",
+          ["<tab>"] = "Toggle",
+          ["x"] = "Discard",
+          ["s"] = "Stage",
+          ["S"] = "StageUnstaged",
+          ["<c-s>"] = "StageAll",
+          ["u"] = "Unstage",
+          ["U"] = "UnstageStaged",
+          ["d"] = "DiffAtFile",
+          ["$"] = "CommandHistory",
+          ["#"] = "Console",
+          ["<c-r>"] = "RefreshBuffer",
+          ["<enter>"] = "GoToFile",
+          ["<c-v>"] = "VSplitOpen",
+          ["<c-x>"] = "SplitOpen",
+          ["<c-t>"] = "TabOpen",
+          ["?"] = "HelpPopup",
+          ["D"] = "DiffPopup",
+          ["p"] = "PullPopup",
+          ["r"] = "RebasePopup",
+          ["m"] = "MergePopup",
+          ["P"] = "PushPopup",
+          ["c"] = "CommitPopup",
+          ["l"] = "LogPopup",
+          ["v"] = "RevertPopup",
+          ["Z"] = "StashPopup",
+          ["A"] = "CherryPickPopup",
+          ["b"] = "BranchPopup",
+          ["f"] = "FetchPopup",
+          ["X"] = "ResetPopup",
+          ["M"] = "RemotePopup",
+          ["{"] = "GoToPreviousHunkHeader",
+          ["}"] = "GoToNextHunkHeader",
         },
       },
     },
