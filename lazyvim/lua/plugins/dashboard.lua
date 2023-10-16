1f80959922faf35a0ae6e49f171dd9aec99161c3
Morten Hersson <mhersson@gmail.com>
2023-10-16 03:58:50 +0200

    LazyVIM: Dashboard `c` now opens telescope and not the init file

--- lazyvim/lua/plugins/dashboard.lua
+++ lazyvim/lua/plugins/dashboard.lua
@@ -35,6 +35,8 @@ return {
     ]]
 
       logo = string.rep("\n", 8) .. logo .. "\n\n"
+
+      local util = require("lazyvim.util")
       local opts = {
         theme = "doom",
         hide = {
@@ -50,7 +52,7 @@ return {
             { action = "ene | startinsert", desc = " New file", icon = " ", key = "n" },
             { action = "Telescope oldfiles", desc = " Recent files", icon = " ", key = "r" },
             { action = "Telescope live_grep", desc = " Find text", icon = " ", key = "g" },
-            { action = "e $MYVIMRC", desc = " Config", icon = " ", key = "c" },
+            { action = util.telescope.config_files(), desc = " Config", icon = " ", key = "c" },
             { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
             { action = "LazyExtras", desc = " Lazy Extras", icon = " ", key = "x" },
             { action = "Lazy", desc = " Lazy", icon = "󰒲 ", key = "l" },
