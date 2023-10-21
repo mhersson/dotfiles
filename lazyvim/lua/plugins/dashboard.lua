3c353608af8a5df3dea2b5c2d157121f0639d176
Morten Hersson <mhersson@gmail.com>
2023-10-21 11:56:35 +0200

    LazyVIM: Minor dashboard updates

--- lazyvim/lua/plugins/dashboard.lua
+++ lazyvim/lua/plugins/dashboard.lua
@@ -36,7 +36,6 @@ return {
 
       logo = string.rep("\n", 8) .. logo .. "\n\n"
 
-      local util = require("lazyvim.util")
       local opts = {
         theme = "doom",
         hide = {
@@ -46,18 +45,19 @@ return {
         },
         config = {
           header = vim.split(logo, "\n"),
-          center = {
-            { action = "Telescope find_files", desc = " Find file", icon = " ", key = "f" },
-            { action = "Telescop project", desc = " Find project", icon = " ", key = "p" },
-            { action = "ene | startinsert", desc = " New file", icon = " ", key = "n" },
-            { action = "Telescope oldfiles", desc = " Recent files", icon = " ", key = "r" },
-            { action = "Telescope live_grep", desc = " Find text", icon = " ", key = "g" },
-            { action = util.telescope.config_files(), desc = " Config", icon = " ", key = "c" },
-            { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
-            { action = "LazyExtras", desc = " Lazy Extras", icon = " ", key = "x" },
-            { action = "Lazy", desc = " Lazy", icon = "󰒲 ", key = "l" },
-            { action = "qa", desc = " Quit", icon = " ", key = "q" },
-          },
+        -- stylua: ignore
+        center = {
+          { action = "Telescope find_files",                                     desc = " Find file",       icon = " ", key = "f" },
+          { action = "Telescop project",                                         desc = " Find project",    icon = " ", key = "p" },
+          { action = "ene | startinsert",                                        desc = " New file",        icon = " ", key = "n" },
+          { action = "Telescope oldfiles",                                       desc = " Recent files",    icon = " ", key = "r" },
+          { action = "Telescope live_grep",                                      desc = " Find text",       icon = " ", key = "g" },
+          { action = [[lua require("lazyvim.util").telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
+          { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
+          { action = "LazyExtras",                                               desc = " Lazy Extras",     icon = " ", key = "x" },
+          { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
+          { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
+        },
           footer = function()
             local stats = require("lazy").stats()
             local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
@@ -68,6 +68,7 @@ return {
 
       for _, button in ipairs(opts.config.center) do
         button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+        button.key_format = "  %s"
       end
 
       -- close Lazy and re-open when the dashboard is ready
