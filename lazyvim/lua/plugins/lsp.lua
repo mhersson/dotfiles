0465d3136dcd3116c602ef7d565b3a514d95653a
Morten Hersson <mhersson@gmail.com>
2023-10-21 11:57:02 +0200

    LazyVIM: Add custom JSON schemas

--- lazyvim/lua/plugins/lsp.lua
+++ lazyvim/lua/plugins/lsp.lua
@@ -1,11 +1,32 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
     opts = {
-      ---@type lspconfig.options
       servers = {
         yamlls = {
+          -- lazy-load schemastore when needed
+          on_new_config = function(new_config)
+            new_config.settings.yaml.schemas = vim.tbl_deep_extend(
+              "force",
+              new_config.settings.yaml.schemas or {},
+              require("schemastore").yaml.schemas({
+                extra = {
+                  {
+                    description = "Mario generator API v3 schema",
+                    fileMatch = "generator.yaml",
+                    name = "Mario generator.yaml",
+                    url = "https://mario-infra.release.rnd.cue.cloud/schema/generator-schema.json",
+                  },
+                  {
+                    description = "Mario component schema",
+                    fileMatch = "components/*.yaml",
+                    name = "Mario components",
+                    url = "https://mario-infra.release.rnd.cue.cloud/schema/component-schema.json",
+                  },
+                },
+              })
+            )
+          end,
           settings = {
             yaml = {
               -- disable formatting because it's handled by yamlfmt
