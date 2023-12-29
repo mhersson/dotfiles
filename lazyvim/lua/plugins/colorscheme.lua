5b969363632ac5e71ef8b5e84de882553d965251
Morten Hersson <mhersson@gmail.com>
2023-12-29 14:33:11 +0100

    LazyVim: add solarized theme

--- lazyvim/lua/plugins/colorscheme.lua
+++ lazyvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,18 @@
 return {
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "catppuccin-macchiato",
+  --   },
+  -- },
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin-macchiato",
-    },
+    "maxmx03/solarized.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.o.background = "light" -- or 'dark'
+
+      vim.cmd.colorscheme("solarized")
+    end,
   },
 }
