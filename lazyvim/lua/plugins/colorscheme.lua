c588aca586d7d243175b5782bb1a517b6f13035b
Morten Hersson <mhersson@gmail.com>
2024-02-29 16:30:43 +0100

    LazyVim: back to catppuccin

--- lazyvim/lua/plugins/colorscheme.lua
+++ lazyvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,18 @@
 return {
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "catppuccin-macchiato",
-  --   },
-  -- },
   {
-    "maxmx03/solarized.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.o.background = "light" -- or 'dark'
-
-      vim.cmd.colorscheme("solarized")
-    end,
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
   },
+  -- {
+  --   "maxmx03/solarized.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.o.background = "light" -- or 'dark'
+  --
+  --     vim.cmd.colorscheme("solarized")
+  --   end,
+  -- },
 }
