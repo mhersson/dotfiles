326df22b1c812fa57be53444d191f75423a28987
Morten Hersson <mhersson@gmail.com>
2024-04-21 13:13:05 +0200

    Waybar: nixos fixes

--- waybar/vpn.sh
+++ waybar/vpn.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 
-vpnip=$(piactl get vpnip)
-class="up"
-tooltip="VPN connected: ${vpnip}"
+if (command -v piactl >/dev/null); then
+  vpnip=$(piactl get vpnip)
+  class="up"
+  tooltip="VPN connected: ${vpnip}"
 
-if (piactl get connectionstate | grep -i disconnected >/dev/null); then
+  if (piactl get connectionstate | grep -i disconnected >/dev/null); then
+    class="down"
+    tooltip="VPN disconnected"
+  fi
+
+else
+
+  # For NixOS using wg-quick
   class="down"
   tooltip="VPN disconnected"
+  ip=$(sudo wg show | grep endpoint | awk '{split($0, a, ":"); print a[2]}')
+  if [ "${ip}" != "" ]; then
+    class="up"
+    tooltip="VPN connected: ${ip}"
+  fi
 fi
 
 echo "{\"class\":\"$class\", \"tooltip\":\"$tooltip\"}"
