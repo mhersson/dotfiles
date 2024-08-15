a4a2013b0944e7fdd6477db7997f0148c679e871
Morten Hersson <mhersson@gmail.com>
2024-08-15 17:16:13 +0200

    Move markdown-preview to separate file
    Also try to stop mason from downloading the internet automatically

--- lazyvim/lua/plugins/nixos.lua
+++ lazyvim/lua/plugins/nixos.lua
@@ -1,18 +1,17 @@
 -- Custom settings just for NixOS - comment out this file in not on NixOS
 return {
-  -- {
-  --   "williamboman/mason.nvim",
-  --   opts = function(_, opts)
-  --     opts.ensure_installed = opts.ensure_installed or {}
-  --     for i, v in ipairs(opts.ensure_installed) do
-  --       if v == "marksman" then
-  --         table.remove(opts.ensure_installed, i)
-  --         break
-  --       end
-  --     end
-  --     vim.list_extend(opts.ensure_installed, { "gopls", "golangci-lint" })
-  --   end,
-  -- },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      for i, v in ipairs(opts.ensure_installed) do
+        -- if v == "marksman" then
+        table.remove(opts.ensure_installed, i)
+        --   break
+        -- end
+      end
+    end,
+  },
   {
     "neovim/nvim-lspconfig",
     opts = {
@@ -23,21 +22,5 @@ return {
         -- rust_analyzer = { mason = false },
       },
     },
-  },
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "npm install",
-    keys = {
-      {
-        "<leader>cp",
-        ft = "markdown",
-        "<cmd>MarkdownPreviewToggle<cr>",
-        desc = "Markdown Preview",
-      },
-    },
-    config = function()
-      vim.cmd([[do FileType]])
-    end,
   },
 }
