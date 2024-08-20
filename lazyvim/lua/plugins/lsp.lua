dc1c36f74d224c3ca97ff1c8bee93f5b003e871c
Morten Hersson <mhersson@gmail.com>
2024-08-20 05:43:05 +0200

    Lazyvim: Disable Mario jsonschema in yamlls

--- lazyvim/lua/plugins/lsp.lua
+++ lazyvim/lua/plugins/lsp.lua
@@ -14,31 +14,7 @@ return {
         fileMatch = { "cue" },
       },
       servers = {
-        cuepls = {},
         yamlls = {
-          -- lazy-load schemastore when needed
-          on_new_config = function(new_config)
-            new_config.settings.yaml.schemas = vim.tbl_deep_extend(
-              "force",
-              new_config.settings.yaml.schemas or {},
-              require("schemastore").yaml.schemas({
-                extra = {
-                  {
-                    description = "Mario generator API v3 schema",
-                    fileMatch = "generator.yaml",
-                    name = "Mario generator.yaml",
-                    url = "https://mario-infra.release.rnd.cue.cloud/schema/generator-schema.json",
-                  },
-                  {
-                    description = "Mario component schema",
-                    fileMatch = "components/*.yaml",
-                    name = "Mario components",
-                    url = "https://mario-infra.release.rnd.cue.cloud/schema/component-schema.json",
-                  },
-                },
-              })
-            )
-          end,
           settings = {
             yaml = {
               -- disable formatting because it's handled by yamlfmt
