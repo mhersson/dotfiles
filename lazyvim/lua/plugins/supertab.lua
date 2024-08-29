23251f951a3e263635faff74e02a143b590c879a
Morten Hersson <mhersson@gmail.com>
2024-08-29 12:55:35 +0200

    LazyVim: add tabout plugin

--- lazyvim/lua/plugins/supertab.lua
+++ lazyvim/lua/plugins/supertab.lua
@@ -3,12 +3,6 @@ return {
     "hrsh7th/nvim-cmp",
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
       local cmp = require("cmp")
 
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
@@ -20,8 +14,6 @@ return {
             vim.schedule(function()
               vim.snippet.jump(1)
             end)
-          elseif has_words_before() then
-            cmp.complete()
           else
             fallback()
           end
