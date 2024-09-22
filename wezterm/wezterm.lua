6116b30243734756c9efefc7b247821ed33808b4
Morten Hersson <mhersson@gmail.com>
2024-09-22 11:43:08 +0200

    Wezterm: Simpler config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,33 +1,28 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This table will hold the configuration.
 local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
--- set the color scheme:
+config.front_end = "WebGpu"
+
 config.color_scheme = "catppuccin-mocha"
 
--- set initial window size
-config.initial_rows = 24
-config.initial_cols = 80
-
--- set cursor style
 config.default_cursor_style = "BlinkingBar"
 config.cursor_blink_rate = 500
 
--- disable audible bell
 config.audible_bell = "Disabled"
 
-local fonts = require("fonts")
-fonts.apply(config)
+config.font_size = 15
+config.font = wezterm.font({ family = "JetBrainsMono Nerd Font" })
 
-local tabs = require("tabs")
-tabs.apply(config)
+config.pane_focus_follows_mouse = true
+
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = true
+config.show_new_tab_button_in_tab_bar = false
+config.use_fancy_tab_bar = false
 
 return config
