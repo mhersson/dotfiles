752f69d152655c38351bdbccb1955c633b39821d
Morten Hersson <mhersson@gmail.com>
2024-10-12 13:09:12 +0200

    LazyVim: Minor markdown config changes

--- lazyvim/lua/plugins/markdown.lua
+++ lazyvim/lua/plugins/markdown.lua
@@ -1,5 +1,16 @@
 return {
   {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      file_types = { "markdown", "norg", "rmd", "org" },
+      code = {
+        sign = true,
+        width = "full",
+        right_pad = 0,
+      },
+    },
+  },
+  {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     build = "npm install",
