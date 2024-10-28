ca9d4054bea41533abe20c09f5070f2c8565666b
Morten Hersson <mhersson@gmail.com>
2024-10-28 11:50:08 +0100

    LazyVim: Back to tokyonight colorscheme

--- lazyvim/lua/plugins/colorscheme.lua
+++ lazyvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,29 @@
 return {
   {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+    },
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
+    end,
+  },
+
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      -- colorscheme = "catppuccin-mocha",
+      colorscheme = "tokyonight",
     },
   },
+  -- {
+  --   "maxmx03/solarized.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.o.background = "light" -- or 'dark'
+  --
+  --     vim.cmd.colorscheme("solarized")
+  --   end,
+  -- },
 }
