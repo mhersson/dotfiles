d89d25b1e40d7eb8d26c340dcd349be9878fc887
Morten Hersson <mhersson@gmail.com>
2025-03-05 19:51:23 +0100

    Doom: adjust font size

--- doom/config.el
+++ doom/config.el
@@ -72,7 +72,7 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 
-(setq doom-font (font-spec :family "RobotoMono Nerd Font" :size 14 :weight 'Regular))
+(setq doom-font (font-spec :family "RobotoMono Nerd Font" :size 15 :weight 'Regular))
 
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
