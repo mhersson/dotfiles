fe8a716a47577c51fc1336cb12157974d86b25b4
Morten Hersson <mhersson@gmail.com>
2025-03-07 11:23:33 +0100

    LazyVim: Better mpls config with MplsOpenPreview command

--- lazyvim/lua/plugins/mpls.lua
+++ lazyvim/lua/plugins/mpls.lua
@@ -13,11 +13,12 @@ return {
           if not configs.mpls then
             configs.mpls = {
               default_config = {
-                cmd = { "mpls", "--enable-emoji" },
+                cmd = { "mpls", "--no-auto", "--enable-emoji" },
                 filetypes = { "markdown" },
                 single_file_support = true,
                 root_dir = function(startpath)
-                  return vim.fs.dirname(vim.fs.find(".git", { path = startpath or vim.fn.getcwd(), upward = true })[1])
+                  local git_root = vim.fs.find(".git", { path = startpath or vim.fn.getcwd(), upward = true })
+                  return git_root[1] and vim.fs.dirname(git_root[1]) or startpath
                 end,
                 settings = {},
               },
@@ -31,6 +32,32 @@ live preview of markdown files in your browser while you edit them in your favor
             }
           end
           lspconfig.mpls.setup(opts)
+          vim.api.nvim_create_user_command('MplsOpenPreview', function()
+            local clients = vim.lsp.get_active_clients()
+            local mpls_client = nil
+
+            for _, client in ipairs(clients) do
+              if client.name == "mpls" then
+                mpls_client = client
+                break
+              end
+            end
+
+            -- Only execute the command if the MPLS client is found
+            if mpls_client then
+              local params = {
+                command = 'open-preview',
+                arguments = {}
+              }
+              mpls_client.request('workspace/executeCommand', params, function(err, result)
+                if err then
+                  print("Error executing command: " .. err.message)
+                end
+              end)
+            else
+              print("mpls is not attached to the current buffer.")
+            end
+          end, {})
         end,
       },
     },
