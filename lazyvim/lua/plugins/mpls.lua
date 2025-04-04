b46522bd12d0ea5312762dd9f8fdc57aad5958b1
Morten Hersson <mhersson@gmail.com>
2025-04-04 09:08:32 +0200

    LazyVim: update mpls with focus switching

--- lazyvim/lua/plugins/mpls.lua
+++ lazyvim/lua/plugins/mpls.lua
@@ -10,6 +10,38 @@ return {
           local lspconfig = require("lspconfig")
           local configs = require("lspconfig.configs")
 
+          local debounce_timer = nil
+          local debounce_delay = 300  -- Delay in milliseconds
+
+          local function sendMessageToLSP()
+              -- Clear the previous timer if it exists
+              if debounce_timer then
+                  debounce_timer:stop()
+              end
+
+              -- Create a new timer
+              debounce_timer = vim.loop.new_timer()
+              debounce_timer:start(debounce_delay, 0, vim.schedule_wrap(function()
+                  local bufnr = vim.api.nvim_get_current_buf()
+                  local clients = vim.lsp.get_active_clients()
+
+                  for _, client in ipairs(clients) do
+                      if client.name == "mpls" then
+                          client.request('mpls/editorDidChangeFocus', { uri = vim.uri_from_bufnr(bufnr) }, function(err, result)
+                          end, bufnr)
+                      end
+                  end
+              end))
+          end
+
+          -- Create an autocommand group
+          local group = vim.api.nvim_create_augroup("MarkdownFocus", { clear = true })
+          vim.api.nvim_create_autocmd("BufEnter", {
+            pattern = "*.md",
+            callback = sendMessageToLSP,
+            group = group,
+          })
+
           if not configs.mpls then
             configs.mpls = {
               default_config = {
