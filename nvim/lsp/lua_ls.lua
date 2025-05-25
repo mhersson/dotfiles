f05cf91f1badd3a5d46747306a08d87a6bf3698f
Morten Hersson <mhersson@gmail.com>
2025-05-25 13:35:55 +0200

    Neovim: add some neovim settings to lua_ls

--- nvim/lsp/lua_ls.lua
+++ nvim/lsp/lua_ls.lua
@@ -11,4 +11,15 @@ return {
         "selene.yml",
         ".git",
     },
+    settings = {
+        Lua = {
+            runtime = { version = "LuaJIT" },
+            diagnostics = { globals = { "vim" } },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false,
+            },
+            telemetry = { enable = false },
+        },
+    },
 }
