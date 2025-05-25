b3544f3bedcf00bd610f52eebaba9b45575bf264
Morten Hersson <mhersson@gmail.com>
2025-05-25 07:33:01 +0200

    Neovim: changed keybinding to toggle Copilot on/off

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -50,7 +50,7 @@ return {
         },
         keys = {
             {
-                "<leader>ac",
+                "<leader>at",
                 function()
                     vim.cmd("Copilot toggle")
                 end,
