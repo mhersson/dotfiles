2af1b11b621f35542c7cc1932e29804e9d1dcfae
Morten Hersson <mhersson@gmail.com>
2025-05-25 12:58:05 +0200

    Neovim: disable node, python3, perl and ruby providers

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,11 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+vim.g.loaded_node_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+
 local opt = vim.opt
 
 opt.autowrite = true -- Enable auto write
