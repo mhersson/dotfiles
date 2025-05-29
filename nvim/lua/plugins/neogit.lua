426f9bfa6164f6a61792b0f85477777a2518d01a
Morten Hersson <mhersson@gmail.com>
2025-05-29 11:20:40 +0200

    Neovim: add some optional dependencies to neogit

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -3,6 +3,8 @@ return {
         "NeogitOrg/neogit",
         dependencies = {
             "nvim-lua/plenary.nvim",
+            "sindrets/diffview.nvim",
+            "folke/snacks.nvim",
         },
         keys = {
             {
