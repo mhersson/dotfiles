ebe9f28761dc462bb2695a4f237462a040585b6c
Morten Hersson <mhersson@gmail.com>
2026-03-19 08:25:23 +0100

    nvim(catppuccin): Remove catppuccin plugin since the theme is now built in

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,10 @@
 local ACTIVE_THEME = "everforest"
 
+-- Catppuccin now comes built in, so no plugin needed
+if ACTIVE_THEME == "catppuccin" then
+    vim.cmd.colorscheme("catppuccin")
+end
+
 return {
     {
         "mhersson/everforest-dark-nvim",
@@ -29,62 +34,11 @@ return {
         end,
     },
     {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        lazy = ACTIVE_THEME ~= "catppuccin",
-        priority = ACTIVE_THEME == "catppuccin" and 1000 or nil,
-        config = function()
-            require("catppuccin").setup({
-                flavor = "mocha",
-                transparent_background = false,
-            })
-            if ACTIVE_THEME == "catppuccin" then
-                vim.cmd.colorscheme("catppuccin")
-            end
-        end,
-        opts = {
-            integrations = {
-                aerial = false,
-                alpha = false,
-                cmp = false,
-                dashboard = false,
-                dap = true,
-                dap_ui = true,
-                flash = false,
-                fzf = true,
-                grug_far = true,
-                gitsigns = true,
-                headlines = false,
-                illuminate = false,
-                indent_blankline = { enabled = true },
-                leap = false,
-                lsp_trouble = true,
-                mason = false,
-                markdown = true,
-                mini = true,
-                native_lsp = {
-                    enabled = true,
-                },
-                navic = { enabled = true, custom_bg = "lualine" },
-                neotest = true,
-                neotree = false,
-                noice = true,
-                notify = true,
-                semantic_tokens = true,
-                snacks = true,
-                telescope = false,
-                treesitter = true,
-                treesitter_context = true,
-                which_key = true,
-            },
-        },
-    },
-    {
         "ellisonleao/gruvbox.nvim",
         lazy = ACTIVE_THEME ~= "gruvbox",
         priority = ACTIVE_THEME == "gruvbox" and 1000 or nil,
         opts = {
-            contrast = "hard",
+            contrast = "",
         },
         config = function(_, opts)
             require("gruvbox").setup(opts)
@@ -109,7 +63,7 @@ return {
         "folke/tokyonight.nvim",
         lazy = ACTIVE_THEME ~= "tokyonight",
         priority = ACTIVE_THEME == "tokyonight" and 1000 or nil,
-        opts = { style = "moon", transparent = false },
+        opts = { style = "night", transparent = false },
         config = function(_, opts)
             require("tokyonight").setup(opts)
             if ACTIVE_THEME == "tokyonight" then
