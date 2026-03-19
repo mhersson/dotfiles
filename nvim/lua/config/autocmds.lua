6274fb197443136d61408af1ff76bfcc56fff003
Morten Hersson <mhersson@gmail.com>
2026-03-19 08:26:52 +0100

    feat(nvim): Switch to using the mpls config from nvim-lspconfig

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -71,51 +71,51 @@ vim.api.nvim_create_autocmd("LspAttach", {
 })
 
 -- MPLS Focus Handler
-local function create_debounced_mpls_sender(delay)
-    delay = delay or 300
-    local timer = nil
-
-    return function()
-        if timer then
-            timer:close()
-            timer = nil
-        end
-
-        timer = vim.uv.new_timer()
-        if not timer then
-            return
-        end
-
-        timer:start(
-            delay,
-            0,
-            vim.schedule_wrap(function()
-                if timer then
-                    timer:close()
-                    timer = nil
-                end
-
-                local bufnr = vim.api.nvim_get_current_buf()
-                if vim.bo[bufnr].filetype ~= "markdown" then
-                    return
-                end
-
-                local clients = vim.lsp.get_clients({ name = "mpls", bufnr = bufnr })
-                if #clients == 0 then
-                    return
-                end
-
-                clients[1]:notify("mpls/editorDidChangeFocus", {
-                    uri = vim.uri_from_bufnr(bufnr),
-                })
-            end)
-        )
-    end
-end
-
-vim.api.nvim_create_autocmd("BufEnter", {
-    pattern = "*.md",
-    callback = create_debounced_mpls_sender(300),
-    group = vim.api.nvim_create_augroup("MplsFocus", { clear = true }),
-    desc = "Notify MPLS of buffer focus changes",
-})
+-- local function create_debounced_mpls_sender(delay)
+--     delay = delay or 300
+--     local timer = nil
+--
+--     return function()
+--         if timer then
+--             timer:close()
+--             timer = nil
+--         end
+--
+--         timer = vim.uv.new_timer()
+--         if not timer then
+--             return
+--         end
+--
+--         timer:start(
+--             delay,
+--             0,
+--             vim.schedule_wrap(function()
+--                 if timer then
+--                     timer:close()
+--                     timer = nil
+--                 end
+--
+--                 local bufnr = vim.api.nvim_get_current_buf()
+--                 if vim.bo[bufnr].filetype ~= "markdown" then
+--                     return
+--                 end
+--
+--                 local clients = vim.lsp.get_clients({ name = "mpls", bufnr = bufnr })
+--                 if #clients == 0 then
+--                     return
+--                 end
+--
+--                 clients[1]:notify("mpls/editorDidChangeFocus", {
+--                     uri = vim.uri_from_bufnr(bufnr),
+--                 })
+--             end)
+--         )
+--     end
+-- end
+--
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--     pattern = "*.md",
+--     callback = create_debounced_mpls_sender(300),
+--     group = vim.api.nvim_create_augroup("MplsFocus", { clear = true }),
+--     desc = "Notify MPLS of buffer focus changes",
+-- })
